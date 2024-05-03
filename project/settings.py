0a6d70d4c533218eb481080c7362c52185e81de1
AwaisRep <syedawais.213shah@gmail.com>
2024-05-04 00:43:47 +0100

    changes to settings

--- project/settings.py
+++ project/settings.py
@@ -94,6 +94,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:8000",
     "http://127.0.0.1:5173",
+    "https://trivela-trivia.onrender.com"
 ]
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:8000",
