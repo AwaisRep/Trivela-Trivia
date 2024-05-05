07d1e9415744e3b3beb40b664cd0fea54c63d920
AwaisRep <syedawais.213shah@gmail.com>
2024-05-05 02:14:22 +0100

    pre data changed

--- load_initial_data.py
+++ load_initial_data.py
@@ -8,6 +8,6 @@ from api.models import DataLoadStatus
 
 if not DataLoadStatus.objects.exists(): # Make sure data has not been loaded before
     # Run loaddata command
-    os.system('python manage.py loaddata data.json')
+    os.system('python manage.py loaddata pre-data.json')
     # Create a DataLoadStatus instance once finished
     DataLoadStatus.objects.create(data_loaded=True)
