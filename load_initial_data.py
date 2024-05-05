995b75c7014e0953656a0e67e72046db20e8a5f7
AwaisRep <syedawais.213shah@gmail.com>
2024-05-05 03:03:16 +0100

    pre-data changes

--- load_initial_data.py
+++ load_initial_data.py
@@ -1,5 +1,6 @@
 import os
 import django
+import glob
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 django.setup()
@@ -7,7 +8,18 @@ django.setup()
 from api.models import DataLoadStatus
 
 if not DataLoadStatus.objects.exists(): # Make sure data has not been loaded before
-    # Run loaddata command
-    os.system('python manage.py loaddata pre-data.json')
-    # Create a DataLoadStatus instance once finished
-    DataLoadStatus.objects.create(data_loaded=True)
+    try:
+        # Get a list of all JSON files in the bank_data directory
+        json_files = glob.glob('bank_data/*.json')
+
+        # Run loaddata command for each file
+        for file in json_files:
+            os.system(f'python manage.py loaddata {file}')
+            print(f'Successfully loaded data from {file}')
+
+        # Create a DataLoadStatus instance once finished to prevent from occuring again
+        DataLoadStatus.objects.create(data_loaded=True)
+        print('Successfully created DataLoadStatus instance')
+
+    except Exception as e:
+        print(f'An error occurred: {e}')
